87bd301e04a3bf513dfe08acbd7ca0767180cd64
mahadihasan999 <mahadihasan445541@gmail.com>
2021-10-27 12:50:28 +0600

    mahaddi

--- index.js
+++ index.js
@@ -58,6 +58,32 @@ app.get('/', (req, res) => {
     res.send(' Running Genius Server');
 })
 
+
+app.get('/hello', (req, res) => {
+
+    res.send('updated succesfuly')
+})
 app.listen(port, () => {
     console.log('Running G Server on Port', port)
 })
+
+/*
+one time:
+1. heroku account open
+2. heroku sotware install
+
+ Every Project
+ 1. git init
+ 2. .gitignore (node_module, .env)
+ 3. push ev3erything to hit
+ 4. make sure you habe this cript: " start": "node index.js"
+ 5. make sure: put process.env.PORT in front of your port number
+ 6. heroku login
+ 7. heroku create(only one tim efo ra porject)
+ 8. command : git push heroku maain
+ ----------
+ update
+ save  everything check locally
+ 1. git add, git commit-m" , git push
+ 2. git push herku main
+*/
